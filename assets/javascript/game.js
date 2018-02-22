eabd876da241752241f4a193a48ff595feed3e26
Melissa Gilman <melissadgilman@yahoo.com>
2018-02-21 18:15:20 -0700

    basic HTML started. Images added.

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// "document.ready" makes sure that our JavaScript doesn't get run until the HTML document is finished loading.
+$(document).ready(function () {
+
+    // VARIABLES
+
+
+    // FUNCTIONS
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //ends the "document.ready" code
